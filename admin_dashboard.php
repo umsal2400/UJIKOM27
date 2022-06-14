9bd8d8e9d6c610c0a2a7168bac330ac58f03bedf
umsal2400 <107489262+umsal2400@users.noreply.github.com>
2022-06-14 21:42:01 +0700

    Add files via upload

---
+++ admin_dashboard.php
@@ -0,0 +1,20 @@
+ <!DOCTYPE html>
+<html lang = "en">
+<head>
+	 <meta charset="utf-8">
+	 <meta http-equiv="X-UA-Compatible" content="IE-edge">
+	 <meta name="viewport" content="width-device-width, initial-scale= 1.0">
+	 <title>ADMIN DASHBOARD</title>
+</head>
+<body>
+	<H1>HALAMAN DASHBOARD</H1>
+	<ul>
+		<li> <a href="dosen_list.php">Data Dosen</a></li>
+		<li> <a href="mk_list.php">Mata Matakuliah</a></li>
+		<li> <a href="jadwal_list.php">Jadwal</a></li>
+	</ul>
+	<ul>
+		<li>Logout</li>
+	</ul>
+</body>
+</html>
